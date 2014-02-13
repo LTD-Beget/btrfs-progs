1309d2d93dc3806cd7a8e2d86b4b0e90f879afed
Goffredo Baroncelli <kreijack@libero.it>
2014-02-13 20:19:01 +0100

    btrfs-progs: Enhance the command btrfs filesystem df
    Enhance the command "btrfs filesystem df" to show space usage information
    for a mount point(s). It shows also an estimation of the space available,
    on the basis of the current one used.
    
    Signed-off-by: Goffredo Baroncelli <kreijack@inwind.it>
    Signed-off-by: David Sterba <dsterba@suse.cz>

--- utils.c
+++ utils.c
@@ -38,6 +38,8 @@
 #include <linux/kdev_t.h>
 #include <limits.h>
 #include <blkid/blkid.h>
+#include <sys/vfs.h>
+
 #include "kerncompat.h"
 #include "radix-tree.h"
 #include "ctree.h"
@@ -2438,4 +2440,14 @@ int test_isdir(const char *path)
 		return -1;
 
 	return S_ISDIR(st.st_mode);
+}
+
+u64 disk_size(char *path)
+{
+	struct statfs sfs;
+
+	if (statfs(path, &sfs) < 0)
+		return 0;
+	else
+		return sfs.f_bsize * sfs.f_blocks;
 }
